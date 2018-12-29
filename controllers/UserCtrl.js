86873037b0c7252d184c1f58d437d2416bfd019e
destiny_123 <akrati.chaturvedi_cs16@gla.ac.in>
2018-12-29 14:20:53 +0530

    updated on 29/12/18

--- controllers/UserCtrl.js
+++ controllers/UserCtrl.js
@@ -7,25 +7,72 @@ module.exports = {
 
         var current = this.currentUser.udaaan_id;
         var userdetailstr = 'select * from member where udaaan_id="%s"';
-        var userdetailquery = util.format(userdetailstr,current); 
+        var userdetailquery = util.format(userdetailstr,current);
         var userdetailresult = yield databaseUtils.executeQuery(userdetailquery);
         var userdetails = userdetailresult[0];
-        console.log(userdetails);
-        
+
+        var teachingstr = 'select teaching_days from member where udaaan_id="%s"';
+        var teachingquery = util.format(teachingstr,current);
+        var teachingresult = yield databaseUtils.executeQuery(teachingquery);
+        console.log(teachingresult);
+        console.log("*********");
+        var teachingdetails = teachingresult[0];
+        console.log(teachingdetails);
+        console.log("*********");
+
+        var d = new Date();
+  var weekday = new Array(7);
+  weekday[0] = "Sun";
+  weekday[1] = "Mon";
+  weekday[2] = "Tue";
+  weekday[3] = "Wed";
+  weekday[4] = "Thur";
+  weekday[5] = "Fri";
+  weekday[6] = "Sat";
+  var n = weekday[d.getDay()];
+            console.log(n);
+            console.log("*********");
+      //  var splits = teachingdetails.split(",");
+      //  for (var i=0;i<splits.length;i++)
+      //  {
+      //    console.log(splits[i]);
+        //}
+
+      //  var teachingsplit = teachingresult.split(',');
+      //  var teachingdetails = teachingsplit[0];
+
+        var updatedetailstr = 'select * from updates limit 5';
+        var updatedetailresult = yield databaseUtils.executeQuery(updatedetailstr);
 
 
+        //Members For Today's Class
+        var memberdetailstr = 'select * from member where teaching_days like "%%%s%"';
+        var memberdetailquery = util.format(memberdetailstr,n);
+        console.log(memberdetailquery);
+        console.log("*********");
+        var memberdetailresult = yield databaseUtils.executeQuery(memberdetailquery);
+        console.log(memberdetailresult);
+        console.log("*********");
+        var memberdetails = memberdetailresult[0];
 
 
         yield this.render('user',{
 
             userdetails:userdetails,
-            currentlist:userdetailresult
-            
+            currentlist:userdetailresult,
+
+            teachingdetails:teachingdetails,
+            teachinglist:teachingresult,
+
+            updatedetailresult:updatedetailresult,
+            updatelist:updatedetailresult,
+
+            memberdetails:memberdetails,
+            memberlist:memberdetailresult
 
         });
     },
 
-    
     logout: function* (next) {
         var sessionId = this.cookies.get("SESSION_ID");
         if(sessionId) {
--- db.sql
+++ db.sql
@@ -34,17 +34,17 @@ primary key(id,udaaan_id)
 );
 
 
-insert into member(udaaan_id,university_id,password,name,email_id,course,department,year,year_of_joining,gender,type,clg_timing,teaching_days,responsibility,blood_group) values
-('udaaan160001','161500048','12345','Akrati Chaturvedi','akrati.chaturvedi_cs16@gla.ac.in','B.Tech','CSE','3','2016','M','Day Scholar','10-6','Mon,Tue,Wed','Acadmics','A+'),
-('udaaan160002','161400018','12345','Pooja Rajora','poojarajora1998@gmail.com','B.Tech','EE','3','2016','F','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
-('udaaan160003','161500210','12345','Gagan Saxena','gagan.saxena_cs16@gla.ac.in','B.Tech','CSE','3','2016','M','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
-('udaaan160004','161500091','12345','Ankita Raj','ankita.gla1_cs16@gla.ac.in','B.Tech','CSE','3','2016','F','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
-('udaaan160005','161500054','12345','Akshay Kumar Katiha','akshay.katiha_cs16@gla.ac.in','B.Tech','CSE','3','2016','M','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
-('udaaan160006','161200166','12345','Rahul Tyagi','rahultyyagi166@gmail.com','B.Tech','ME','3','2016','M','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
-('udaaan160007','161500083','12345','Anjali Goyal','ani.goyal169@gmail.com','B.Tech','CSE','3','2016','F','Day Scholar','10-6','Mon,Tue,Wed','Acadmics','A+'),
-('udaaan160008','161500592','12345','Udit saxena','saxenaudit56@gmail.com','B.Tech','CSE','3','2016','M','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
-('udaaan160009','161200158','12345','Praveen Sangam Gupta','psg1997gkp@gmail.com','B.Tech','ME','3','2016','M','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
-('udaaan160010','161500534','12345','Shraddha Jaiswal','shraddha.jaiswal_cs16@gla.ac.in','B.Tech','CSE','3','2016','F','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+');
+insert into member(udaaan_id,university_id,password,name,photo,email_id,course,department,year,year_of_joining,gender,type,clg_timing,teaching_days,responsibility,blood_group) values
+('udaaan160001','161500048','12345','Akrati Chaturvedi','/static/images/akrati.jpg','akrati.chaturvedi_cs16@gla.ac.in','B.Tech','CSE','3','2016','M','Day Scholar','10-6','Mon,Tue,Sat','Acadmics','A+'),
+('udaaan160002','161400018','12345','Pooja Rajora','/static/images/user.png','poojarajora1998@gmail.com','B.Tech','EE','3','2016','F','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
+('udaaan160003','161500210','12345','Gagan Saxena','/static/images/user.png','gagan.saxena_cs16@gla.ac.in','B.Tech','CSE','3','2016','M','Hosteler','10-6','Mon,Tue,Sat','Acadmics','A+'),
+('udaaan160004','161500091','12345','Ankita Raj','/static/images/user.png','ankita.gla1_cs16@gla.ac.in','B.Tech','CSE','3','2016','F','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
+('udaaan160005','161500054','12345','Akshay Kumar Katiha','/static/images/user.png','akshay.katiha_cs16@gla.ac.in','B.Tech','CSE','3','2016','M','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
+('udaaan160006','161200166','12345','Rahul Tyagi','/static/images/user.png','rahultyyagi166@gmail.com','B.Tech','ME','3','2016','M','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
+('udaaan160007','161500083','12345','Anjali Goyal','/static/images/user.png','ani.goyal169@gmail.com','B.Tech','CSE','3','2016','F','Day Scholar','10-6','Mon,Sat,Wed','Acadmics','A+'),
+('udaaan160008','161500592','12345','Udit saxena','/static/images/user.png','saxenaudit56@gmail.com','B.Tech','CSE','3','2016','M','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
+('udaaan160009','161200158','12345','Praveen Sangam Gupta','/static/images/user.png','psg1997gkp@gmail.com','B.Tech','ME','3','2016','M','Hosteler','10-6','Mon,Tue,Wed','Acadmics','A+'),
+('udaaan160010','161500534','12345','Shraddha Jaiswal','/static/images/user.png','shraddha.jaiswal_cs16@gla.ac.in','B.Tech','CSE','3','2016','F','Hosteler','10-6','Mon,Tue,Sat','Acadmics','A+');
 
 
 
@@ -57,7 +57,7 @@ id int unsigned not null auto_increment,
 udaaan_id varchar(20) not null unique,
 name varchar(60),
 father_name varchar(60),
-mother_name varchar(60), 
+mother_name varchar(60),
 address varchar(300),
 contact_no varchar(20),
 class varchar(10),
@@ -93,3 +93,23 @@ creation_timestamp datetime default CURRENT_TIMESTAMP,
 primary key(id,udaaan_id)
 
 );
+
+
+create table updates
+(
+id int unsigned not null auto_increment,
+udaaan_id varchar(20) not null,
+update_title varchar(30),
+update_description varchar(200),
+creation_timestamp datetime default CURRENT_TIMESTAMP,
+primary key(id)
+
+);
+
+insert into updates(id,udaaan_id,update_title,update_description) values
+(1,'udaaan160001','Sankalan is coming soon...','It is the alumini meet which is organized annually.'),
+(2,'udaaan160001','Sunday Visit...','Facilis ipsum reprehenderit nemo molestias.'),
+(3,'udaaan160001','Facilis ipsum nemo molestias','Facilis ipsum nemo molestias.'),
+(4,'udaaan160001','Facilis ipsum nemo molestias','Facilis ipsum nemo molestias.'),
+(5,'udaaan160001','Facilis ipsum nemo molestias','Facilis ipsum nemo molestias.'),
+(6,'udaaan160001','Facilis ipsum nemo molestias','Facilis ipsum nemo molestias.');
